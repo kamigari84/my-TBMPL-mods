1f62e5196a5a4707bb10614a720c5feb4cd3f43a
kamigari84 <40356228+kamigari84@users.noreply.github.com>
2024-02-04 03:01:32 +0100

    potential final version
    to be tested

--- Fix Workerless Recipes/fix_hauling_into_recipe.cs
+++ Fix Workerless Recipes/fix_hauling_into_recipe.cs
@@ -2,18 +2,16 @@ using TBMPLCore.Plugin;
 using TBMPLCore.Plugin.Attributes;
 using TBMPLCore.Plugin.Config;
 using HarmonyLib;
-using UnityEngine;
 using System;
 using Timberborn.Workshops;
 using Timberborn.Hauling;
-using TBMPLCore.Plugin.Logs;
-
+using Timberborn.WorkSystem;
 
 
 namespace WorkerlessRecipe_HaulingFix
 {
     [TBMPLVersionCheck("https://github.com/kamigari84/my-TBMPL-mods/raw/update5/Fix%20Workerless%20Recipes/version.json")]
-    [TBMPL(TBMPL.Prefix + "Hauling2RecipeFix", "Improve /Prioritize by Haulers/", "1.0.9.3")]
+    [TBMPL(TBMPL.Prefix + "Hauling2RecipeFix", "Improve /Prioritize by Haulers/", "1.0.9.4")]
     internal sealed class EP : EntryPoint
     {
         public static new EPConfig Config { get; }
@@ -23,11 +21,11 @@ namespace WorkerlessRecipe_HaulingFix
         {
             return new EPConfig
             {
-                Prioritize_threshold = AddKey("/Prioritize by Haulers/ Settings", "threshold", 0.25f, "Base priority threshold for appying prioritization, ignored for No-Worker-needed Workshops if those tweaks are enabled"), // float default value 0.25
-                Prioritize_strength = AddKey("/Prioritize by Haulers/ Settings", "strength", 1f, "Prioritization strength to be applied with ingame toggle"), // float default value 1
-                Workerless_toggle = AddKey("Haul to No-Worker Workshop Settings", "enable", true, "Give special treatment to No-Worker-needed Workshops?"), // bool default value true
-                Workerless_priority = AddKey("Haul to No-Worker Workshop Settings", "priority", 1.11f, "Extra Prioritization strength for workerless manufacturies \n\t (tied to /Prioritize by Haulers/ toggle)"), // float default value 1.11
-                Workerless_floor = AddKey("Haul to No-Worker Workshop Settings", "floor", 0.89f, "Basic (calculated by the HaulCandidate) prioritization weight is always at least ..."), // float default value 0.89
+                Prioritize_threshold = AddKey("/Prioritize by Haulers/ Settings", "threshold", 0.25f, "Base priority threshold for appying prioritization"), // float default value 0.25
+                Prioritize_strength = AddKey("/Prioritize by Haulers/ Settings", "strength", 1.1f, "Prioritization strength to be applied with ingame toggle"), // float default value 1.1
+                Workerless_toggle = AddKey("Workerless Manufactory Hauling Settings", "enable", true, "Give special treatment to Workerless Manufactories?"), // bool default value true
+                Workerless_floor = AddKey("Workerless Manufactory Hauling Settings", "floor", 1.2f, "For Workerless Manufactories, always increase prioritization weight by \n\tshould be >= threshold"), // float default value 1.2
+                Workplace_deprioritize = AddKey("Workerless Manufactory Hauling Settings", "lower", true, "Set Workplace Priority of potentianlly-workerless Manufactories to lowest?"), // bool default value true
 
             };
         }
@@ -37,10 +35,9 @@ namespace WorkerlessRecipe_HaulingFix
     {
         public float Prioritize_threshold { get; set; }
         public float Prioritize_strength { get; set; }
-        public float Workerless_priority { get; set; }
         public float Workerless_floor { get; set; }
-        public bool Workerless_toggle { get; set;}
-
+        public bool Workerless_toggle { get; set; }
+        public bool Workplace_deprioritize { get; set; }
     }
 
 
@@ -49,6 +46,7 @@ namespace WorkerlessRecipe_HaulingFix
         [HarmonyPatch]
         internal static class HaulingPatch
         {
+
             [HarmonyPrefix]
             [HarmonyPatch(typeof(HaulCandidate), "PrioritizeAndValidate", new Type[] {
                typeof(float)
@@ -58,19 +56,37 @@ namespace WorkerlessRecipe_HaulingFix
                 HaulPrioritizable ____haulPrioritizable,
                 ref float __result)
             {
-                bool _workerless = EP.Config.Workerless_toggle && ____haulPrioritizable.GameObjectFast.TryGetComponent<ProductionIncreaser>(out _);
-                bool _prioritize = ____haulPrioritizable.Prioritized && (weight >= EP.Config.Prioritize_threshold || _workerless);
-                Log.Debug("initial weight:" + weight);
-                __result = _workerless ? Mathf.Clamp(weight, Mathf.Max(EP.Config.Prioritize_threshold, EP.Config.Workerless_floor), 5f) : weight;
-                Log.Debug("checked if HaulCandidate is a No-Workers Manufactory("+_workerless+") ... corrected initial value:" + __result);
+                __result = weight;
+                if (__result == 0)
+                {
+                    return false;
+                }
 
-                __result += _prioritize ? (_workerless ? EP.Config.Prioritize_strength + EP.Config.Workerless_priority : EP.Config.Prioritize_strength) : 0f;
-                Log.Debug("checked if HaulCandidate is meant to be Prioritized (" + _prioritize + ") ... tmp value" + __result);
+                if (EP.Config.Workerless_toggle && ____haulPrioritizable.GameObjectFast.TryGetComponent<ProductionIncreaser>(out _))
+                {
+                    __result += EP.Config.Workerless_floor;
+                }
 
-                __result = Mathf.Clamp(__result, 0f, 10f);
-                Log.Debug($"final haul priority for {____haulPrioritizable.GameObjectFast.name}(within 0-10f): {__result}");
+
+                if ((____haulPrioritizable.Prioritized && (__result >= EP.Config.Prioritize_threshold)))
+                {
+                    __result += EP.Config.Prioritize_strength;
+                }
+
                 return false;
             }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(ProductionIncreaser), "OnEnterFinishedState")]
+            private static void DePrioritizer(Manufactory ____manufactory)
+            {
+                if (EP.Config.Workplace_deprioritize && ____manufactory.GameObjectFast.TryGetComponent<WorkplacePriority>(out var priority))
+                {
+                    priority.SetPriority(Timberborn.PrioritySystem.Priority.VeryLow);
+                }
+            }
+
         }
     }
+
 }
---
+++ Fix Workerless Recipes/haul-prioritize.cs
@@ -0,0 +1,56 @@
+using HarmonyLib;
+using System;
+using TBMPLCore.Plugin;
+using TBMPLCore.Plugin.Attributes;
+using TBMPLCore.Plugin.Config;
+using TBMPLCore.Plugin.Logs;
+using Timberborn.Hauling;
+using Timberborn.Workshops;
+
+
+
+namespace WorkerlessRecipe_HaulingFix
+{
+    //    [TBMPLVersionCheck("https://github.com/kamigari84/my-TBMPL-mods/raw/update5/Fix%20Workerless%20Recipes/version.json")]
+    [TBMPL(TBMPL.Prefix + "HaulingImproved", "Improve /Prioritize by Haulers/ LITE", "0.0.1")]
+    internal sealed class EP : EntryPoint
+    {
+        public static new EPConfig Config { get; }
+
+    }
+    internal sealed class EPConfig : BaseConfig { }
+
+
+    namespace Patches
+    {
+        [HarmonyPatch]
+        internal static class HaulingPatch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(HaulCandidate), "PrioritizeAndValidate", new Type[] {
+               typeof(float)
+    })]
+            private static bool HaulCandidate_PrioritizeAndValidate_Patch(
+                float weight,
+                HaulPrioritizable ____haulPrioritizable,
+                ref float __result)
+            {
+                __result = weight;
+                Log.Debug("initial weight:" + __result);
+                if (____haulPrioritizable.GameObjectFast.TryGetComponent<ProductionIncreaser>(out _))
+                {
+                    Log.Debug("is Workerless");
+                    __result = 2f;
+                }
+                if (____haulPrioritizable.Prioritized)
+                {
+                    Log.Debug("is Prioritized");
+                    __result += 1f;
+                }
+                //__result = Mathf.Clamp(__result, 0f, 10f);
+                Log.Debug($"final haul priority for {____haulPrioritizable.GameObjectFast.name}: {__result}");
+                return false;
+            }
+        }
+    }
+}
